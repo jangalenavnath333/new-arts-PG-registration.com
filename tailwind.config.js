afb1ec81f766f9ab3b1804217b6f922c6b069f3d
“Navnath <jangalenavnath333@gmail.com>
2026-06-23 13:57:10 +0530

    UI: Hardcoded button styles to fix Tailwind cache purge bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './*.html',
     './admin/**/*.html',
+    './student/**/*.html',
+    './js/**/*.js',
+    './api/**/*.js'
   ],
   theme: {
     extend: {},
